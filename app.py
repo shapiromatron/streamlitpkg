5e136bfe4d81dd60aa56d9c69d3d6d601948f88d
Andy Shapiro <shapiro.andy@epa.gov>
2025-05-29 13:53:12 -0400

    attempt #2

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-# required to be at repository root for Posit Connect
-from src.streamlitpkg import app  # noqa: F401
+# Replicate bundled app for the purposes of Posit Connect Deployment
+import streamlit as st
+
+from src.streamlitpkg.work import super_add
+
+a = st.slider("Value of A?", 0, 130, 25)
+b = st.slider("Value of B?", 0, 130, 25)
+st.write(f"Super Add: {a} + {b} = {super_add(a, b)}")
